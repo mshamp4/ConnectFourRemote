7ab66cf0c0f760d3b8799c83013b49c5e27977ba
Matthew Shampine <shampinm@mail.gvsu.edu>
2017-04-20 11:50:04 -0400

    Added cfCell functionality to the connect four game

--- src/connectfour/ConnectFourGame.java
+++ src/connectfour/ConnectFourGame.java
@@ -1,5 +1,18 @@
 package connectfour;
 
+/**
+ * This class is the controller of the MVC design, it creates a fully 
+ * functional connect four game board and makes all decisions 
+ * regarding the state of the game.
+ * 
+ * @author Matthew Shampine
+ * @version 1.0
+ */
 public class ConnectFourGame {
+	
+	private cfCell[][] board;
+	
+	
+	
 
 }
---
+++ src/connectfour/cfCell.java
@@ -0,0 +1,92 @@
+package connectfour;
+
+/**
+ * This class holds the properties of a connect four circle. These
+ * properties include which player controls the cell, whether or not 
+ * the cell has been clicked on, and the rating of the cell that is
+ * used to assist the logic of the AI.
+ * 
+ * @author Matthew Shampine
+ * @version 1.0
+ */
+public class cfCell {
+
+	/** A char to represent which player selected the cell. */
+	private char player;
+	
+	/** Boolean to mark whether or not the cell has been selected. */
+	private boolean isMarked;
+	
+	/** An integer to represent how good of a move a cell is. */
+	private int rating;
+	
+	/**
+	 * Constructor that instantiates a new cell and sets up the cell with
+	 * default properties.
+	 * 
+	 */
+	public cfCell() {
+		player = '?';
+		isMarked = false;
+		rating = 0;
+	}
+
+	/**
+	 * Getter method that returns the player that selected that cell.
+	 * 
+	 * @return char The player who currently has the cell selected
+	 */
+	public char getPlayer() {
+		return player;
+	}
+
+	/**
+	 * Setter method that sets the player of a cell.
+	 * 
+	 * @param player
+	 *            The player that is going to be owner of that cell
+	 */
+	public void setPlayer(final char player) {
+		this.player = player;
+	}
+
+	/**
+	 * Getter method that returns whether or not the cell
+	 * has been selected.
+	 * 
+	 * @return boolean If the cell has an owner or not
+	 */
+	public boolean isMarked() {
+		return isMarked;
+	}
+
+	/**
+	 * Setter method that marks the cell as being selected or not.
+	 * 
+	 * @param isMarked
+	 *            Whether or not the cell has been selected
+	 */
+	public void setMarked(final boolean isMarked) {
+		this.isMarked = isMarked;
+	}
+
+	/**
+	 * Getter method that returns the rating of a cell. The higher
+	 * rating a cell has the better the move is.
+	 * 
+	 * @return int The rating of a cell
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * Setter method that adds one to the rating.
+	 * 
+	 * @param rating
+	 *            The current rating of that cell
+	 */
+	public void setRating(final int rating) {
+		this.rating += rating;
+	}
+}
